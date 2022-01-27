e877998e6e0fc76de13f15159271a393e856e38b
Muntasir Tonmoy <muntasir01521566317@gmail.com>
2022-01-27 15:36:36 +0600

    first js

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log(2 + 4 + 6)
+// learning javascript module1 lecture 1
+// Run javascript in vs code
+
+/* what is script language?
+A scripting language is a programming language that is interpreted. It is translated into machine code when the code is run, rather than beforehand. Scripting languages are often used for short scripts over full computer programs. JavaScript, Python, and Ruby are all examples of scripting languages.
+*/
+
+/* what is javascript?
+JavaScript is a scripting or programming language that allows you to implement complex features on web pages
+*/
+
+// We can run javascript in the browser by going inspect>console
+
+//we also can use in codepen.io but we shouldn't use it all the time
+
+//but we will use vs code 
+
+console.log(2 + 2)
