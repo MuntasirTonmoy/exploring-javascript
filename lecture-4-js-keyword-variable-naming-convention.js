2c059b0c87d57c2938750f03f1117143099bfc1f
Muntasir Tonmoy <muntasir01521566317@gmail.com>
2022-01-27 17:48:29 +0600

    lecture 4 variable naming convention

---
+++ lecture-4-js-keyword-variable-naming-convention.js
@@ -0,0 +1,26 @@
+//variable naming convention
+
+//VARIABLE NAME IS CASE SENSITIVE*
+
+//javascript keyword or reserve words is key sensitive
+
+/* rules for naming variable
+
+1. can't use reserve keyword as variable name such as
+var false = 15; is a invalid variable
+
+2. name should be one word but can use more than one word by using underscore _
+ex: var my_name = "Muntasir Tonmoy"; is a valid variable
+var my name = "Muntasir Tonmoy" is invalid.
+but js programmer don't use underscore the normally uppercase the first letter of the next word
+ex: var myName = "Muntasir Tonmoy";
+var myOfficeAddress = "sweet home"; it is called camel case.
+
+3. Quote mark is not allowed in variable name
+ex: var "my name" = "Muntasie Tonmoy"; is invalid
+
+4. can't use numeric in the first latter but can use numeric in 2nd latter or after.
+ex: var 99price = "99"; invalid
+*/
+
+//SUMMERY - name should be one word if need more word then use camel case.
