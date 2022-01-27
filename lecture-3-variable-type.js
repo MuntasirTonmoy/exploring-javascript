4096ef363ff57894ed9c29f9c45b9d271d341f39
Muntasir Tonmoy <muntasir01521566317@gmail.com>
2022-01-27 16:18:52 +0600

    l3 complete

---
+++ lecture-3-variable-type.js
@@ -0,0 +1,20 @@
+//variable type
+
+//Numeric variable means its value is a valid number
+
+var egg_price = 23;
+
+// string variable means when its value contains more than one latter
+// we can write string variabele in three ways first two is simple third one is advance
+// we have to put the value insede double quote "" or single quote ''
+
+var name = "Muntasir Tonmoy";
+var fathers_name = 'Md. Humayun Kabir';
+
+//boolean type means true or false
+// true and false look like string but it is not it is special thats why we dont give quote mark
+
+var pass_in_test = true;
+var password_match = true;
+var subscriber = false;
+
