a7333b1e867dab8fb4c4fe9569c95d47318e2b5b
Muntasir Tonmoy <muntasir01521566317@gmail.com>
2022-01-27 16:00:51 +0600

    l2 complete

---
+++ lecture2-variable.js
@@ -0,0 +1,18 @@
+//vary+able = variable means its value can vary we can store value or information inside variable
+
+//how to write variable
+/* 5 steps to follow for writing a simple variable
+1. first we have to write var
+2. then we have to give a meaningful valid name so that one can easily understand what is this variable for
+3. then we have to write assignment operator =
+4. then put value
+5. give semicolon ; 
+example.
+*/
+
+var price = 25; // it means price 25 
+var age = 23;
+var temparature = 20; // it means temparature 20
+var mypocketmoney = 0; // hehe 
+
+
